5bf38d8ad8566b7fec5c69ea793ee777546b4c28
Thomas Brown <github@honourity.com>
2017-06-21 08:22:33 +1000

    added stats summary UI

---
+++ Assets/Scripts/UI/UIStatsController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UIStatsController : MonoBehaviour
+{
+	public Slider _slider;
+	public Text _wavesCount;
+	public Text _summary;
+
+	private void Start()
+	{
+		_slider.minValue = 0;
+		_slider.maxValue = GameManager.Instance.SecondsBetweenWaves;
+	}
+
+	private void Update()
+	{
+		_slider.value = GameManager.Instance.TimeUntilNextWave;
+
+		_wavesCount.text = (GameManager.Instance.WavesSurvived + 1).ToString();
+
+		_summary.text = string.Format("Defender Deaths: {0}      Buildings Remaining: {1}   Lives Remaining: {2}\nEnemies Per Wave: {3}   Enemies Killed: {4}",
+			GameManager.Instance.DefendersKilled,
+			GameManager.Instance.BuildingsRemaining,
+			GameManager.Instance.ObjectiveLivesRemaining,
+			GameManager.Instance.EnemiesPerWave,
+			GameManager.Instance.EnemiesKilled);
+	}
+}
