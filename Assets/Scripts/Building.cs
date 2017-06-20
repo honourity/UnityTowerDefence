f7cb96f0848da5a3bf7033fe43c70f35dadba59a
Thomas Brown <thomas.la.brown@gmail.com>
2017-06-20 21:04:31 +1000

    created defender, randomly adds defenders to rooftops at game start (can add, remove, and removeall defenders from a building)

---
+++ Assets/Scripts/Building.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class Building : MonoBehaviour {
+
+	public Defender DefenderPrefab;
+	public Transform DefenderSpawn;
+
+	private List<Defender> _defenders;
+
+	public void Awake()
+	{
+		_defenders = new List<Defender>();
+	}
+
+	public void AddDefenders(int numDefenders)
+	{
+		for (int i = 0; i < numDefenders; i++)
+		{
+			if (_defenders.Count < 25)
+			{
+				var spawnPos = DefenderSpawn.transform.position + new Vector3(0.5f, 0.5f, -0.5f);
+				var offsetX = _defenders.Count % 5;
+				var offsetZ = -(_defenders.Count / 5);
+				spawnPos.x += offsetX;
+				spawnPos.z += offsetZ;
+				var defender = Instantiate(DefenderPrefab, spawnPos, DefenderSpawn.transform.rotation, gameObject.transform);
+
+				_defenders.Add(defender);
+			}
+			else
+			{
+				Debug.Log("No defenders added to " + gameObject.name + " because it it full already!");
+				break;
+			}
+		}
+	}
+
+	public void RemoveDefender()
+	{
+		if (_defenders.Count > 0)
+		{
+			var lastDefender = _defenders.Last();
+			_defenders.Remove(lastDefender);
+			Destroy(lastDefender.gameObject);
+		}
+	}
+
+	public void RemoveAllDefenders()
+	{
+		for (int i = 0; i <= _defenders.Count; i++)
+		{
+			RemoveDefender();
+		}
+	}
+}
---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+[DisallowMultipleComponent]
+public class GameManager : MonoBehaviour {
+	public static GameManager Instance
+	{
+		get { return _instance = _instance ?? FindObjectOfType<GameManager>() ?? new GameManager { }; }
+	}
+	private static GameManager _instance;
+
+	private System.Random _random;
+
+	public System.Random Random
+	{
+		get
+		{
+			return _random = _random ?? new System.Random();
+		}
+	}
+
+	public Building[] Buildings;
+
+	public void Awake()
+	{
+		for (int i = 0; i < Buildings.Length; i++)
+		{
+			Buildings[i].AddDefenders(Random.Next(0,3));
+		}
+	}
+}
