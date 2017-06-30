fcdd4e3f529c6bb3955baf1fa0c7ed381df83be5
Thomas Brown <github@honourity.com>
2017-06-30 19:56:39 +1000

    sun direction bound to TimeManager's timescaling

--- Assets/Scripts/Managers/TimeManager.cs
+++ Assets/Scripts/Managers/TimeManager.cs
@@ -8,25 +8,40 @@ public class TimeManager : MonoBehaviour
 	public int Timescale = 1;
 	[Tooltip("In the format: 'h:mm tt' such as '9:00 AM'")]
 	public string StartTime = "9:00 AM";
+	public Light SunlightSource;
 
 	private Text _hourText;
 	private DateTime _time;
 
-	void Start()
+	private void Start()
 	{
 		_hourText = GameObject.Find("HourText").GetComponent<Text>();
-		_time = DateTime.ParseExact(StartTime, "h:mm tt", CultureInfo.InvariantCulture); ;
+		_time = DateTime.ParseExact(StartTime, "h:mm tt", CultureInfo.InvariantCulture);
 	}
 
-	void Update()
+	private void Update()
 	{
 		CalculateTime();
+
+		UpdateSunlight();
 	}
 
-	void CalculateTime()
+	private void UpdateSunlight()
+	{
+		var angle = Map(_time.Ticks % TimeSpan.TicksPerDay, 0, TimeSpan.TicksPerDay, 0, 360);
+
+		SunlightSource.transform.eulerAngles = new Vector3(angle + 90, 20, 20);
+	}
+
+	private void CalculateTime()
 	{
 		_time = _time.AddSeconds(Time.deltaTime * Timescale);
 
 		_hourText.text = _time.ToString("h:mm tt");
+	}
+
+	private float Map(float x, float in_min, float in_max, float out_min, float out_max)
+	{
+		return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 	}
 }
