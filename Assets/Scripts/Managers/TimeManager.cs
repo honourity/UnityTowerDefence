8902fffc903c928369260df01ded18c5cf2331cf
Thomas Brown <github@honourity.com>
2017-06-30 19:02:31 +1000

    minor scene and editor changes to timemanager

--- Assets/Scripts/Managers/TimeManager.cs
+++ Assets/Scripts/Managers/TimeManager.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 public class TimeManager : MonoBehaviour
 {
 	public int Timescale = 1;
-	[Tooltip("In the format: 'hh:mm tt' such as '9:00 AM'")]
+	[Tooltip("In the format: 'h:mm tt' such as '9:00 AM'")]
 	public string StartTime = "9:00 AM";
 
 	private Text _hourText;
